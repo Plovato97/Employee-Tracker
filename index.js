7a5d5ef978dabde1e2e0ab865ba01d1985c17f02
paul lovato <lovato.paul97@yahoo.com>
2022-11-16 13:55:57 -0500

    sql connection

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ connection.connect(async (err) => {
         type: 'list',
         message: 'What would you like to do?',
         choices: [
+          'View all employees?',
+          'View roles?',
+          'View departments?',
           'Add employee?',
           'Add role?',
           'Add department?',
-          'View all employees?',
-          'View role?',
-          'View department?',
           'Update employee role?',
           'Exit?'
         ],
@@ -41,30 +41,75 @@ connection.connect(async (err) => {
 });
 
 
-// const doUsersChoice = async (userChoice) => {
+const selections= async (userChoice) => {
 
-//   if (userChoice === 'Add employee?') {
-//     addEmployee();
-//   }
-//   if (userChoice === 'Add role?') {
-//     addRole();
-//   }
-//   if (userChoice === 'Add department?') {
-//     addDepartment();
-//   }
-//   if (userChoice === 'View all employees?') {
-//     viewEmployee();
-//   }
-//   if (userChoice === 'View role?') {
-//     viewRole();
-//   }
-//   if (userChoice === 'View department?') {
-//     viewDepartment();
-//   }
-//   if (userChoice === 'Update employee role?') {
-//     updateEmpRole();
-//   }
-//   if (userChoice === 'Exit?') {
-//     exit();
-//   }
-// };
+  if (userChoice === 'View all employees?') {
+    viewEmployee();
+  }
+  if (userChoice === 'View roles?') {
+    viewRole();
+  }
+  if (userChoice === 'View departments?') {
+    viewDepartment();
+  }
+  if (userChoice === 'Add employee?') {
+    addEmployee();
+  }
+  if (userChoice === 'Add role?') {
+    addRole();
+  }
+  if (userChoice === 'Add department?') {
+    addDepartment();
+  }
+  if (userChoice === 'Update employee role?') {
+    updateEmpRole();
+  }
+  if (userChoice === 'Exit?') {
+    console.log('Thank you for using Employee Tracker.')
+    exit();
+  }
+};
+
+const viewEmployee = () => {
+  connection.query('SELECT * FROM employees', (err, employees) => {
+    if (err) throw err; 
+    console.table(employees);
+    connection.end();
+  });
+};
+
+const viewRole = () => {
+  connection.query('SELECT * FROM role', (err, role) => {
+    if (err) throw err; 
+    console.table(role);
+    connection.end();
+  });
+};
+
+const viewDepartment= () => {
+  connection.query('SELECT * FROM departments', (err, departments) => {
+    if (err) throw err;
+    console.table(departments);
+    connection.end();
+  });
+};
+
+// const addEmployee = () => {
+//   connection.query
+// }
+
+// const addRole = () => {
+//   connection.query
+// }
+
+// const addDepartment = () => {
+//   connection.query
+// }
+
+// const updateEmpRole = () => {
+//   connection.query
+// }
+
+// function exit() {
+// does this need to exist or can i exit the function from the main prompt??
+// }
--- seed.sql
+++ seed.sql
@@ -4,56 +4,65 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
--- department Table --
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+-- departments table
+CREATE TABLE departments (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
 );
 
--- role Table --
+-- role table
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
--- employees Table 
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+-- employees table
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
 
--- vlaues for department table --
-INSERT INTO department (name)
+-- vlaues for departments table --
+INSERT INTO departments (name)
 VALUES ('Engineer'),
 ('Sales'),
-('Finance'),
+('HR'),
 ('Legal');
 
 -- values for role table --
 INSERT INTO role (title, salary, department_id)
-VALUES ('Lead Engineer', 160000, 3),
-('Engineer', 100000, 4),
-('Sales Lead', 80000, 10),
-('Sales Person', 60000, 11),
-('Lawyer', 180000, 20);
+VALUES ('Lead Engineer', 160000, 1),
+('Engineer', 100000, 1),
+('Sales Lead', 85000, 2),
+('Sales Person', 60000, 2),
+('HR', 95000, 3),
+('Lawyer', 180000, 4);
 
--- values for employee table --
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Tom', 'Segura', 10, null),
-('Joan', 'Rivers', 11, 10),
-('Larry', 'David', 11, 10),
-('Bernie', 'Mac', 20, null),
-('Amy', 'Schumer', 3, null),
-('Carol', 'Burnett', 4, 3),
-('Rodney', 'Dangerfield', 4, 3),
-('Iliza', 'Shlesinger', 20, null),
-('Ali', 'Wong', 10, null),
-('Dave', 'Chappelle', 11, 10);
+
+-- values for employees table --
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('Ali', 'Wong', 1, null),
+('Carol', 'Burnett', 2, 1),
+('Rodney', 'Dangerfield', 2, 1),
+('Amy', 'Schumer', 1, null),
+('Dave', 'Chappelle', 2, 4),
+('Tom', 'Segura', 3, null),
+('Joan', 'Rivers', 4, 6),
+('Larry', 'David', 4, 6),
+('Iliza', 'Shlesinger', 5, null),
+('Bernie', 'Mac', 6, null);
+
+
+-- SELECT * FROM employees;
+-- how do i select to show all the tables together??  
